2aac978203131cefae1d5b365431c7d96323f9c4
Sainath Shinde <shindesainath692@gmail.com>
2026-01-24 08:15:02 +0530

    Handling multiple route handler with next function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/user", (req, res) => {
+  //Route Handler
   res.send("Get User Successfully");
 });
 
@@ -12,6 +13,12 @@ app.post("/user", (req, res) => {
 
 app.delete("/user", (req, res) => {
   res.send("User Created");
+});
+
+app.get("/users", (req, res) => {
+  // res.send("route Handler Called")
+  // If we didnt call rout handleer and with res send then api call in postman goes calling too long and time Out
+  // because We havn't pass any res
 });
 
 app.listen(8080, () => {
