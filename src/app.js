94f01173cab8bdab2968ffe4e173f164a7c680c1
Sainath Shinde <shindesainath692@gmail.com>
2026-01-31 18:53:19 +0530

    get apis implementation

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const users = await User.findOne({ email: userEmail });
+    if (users.length === 0) {
+      res.status(404).send('User not found');
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send('User not found');
+  }
+});
+
+app.get('/userList', async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send('Users data fetched successfully');
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send('User not found');
+  }
+});
+
 connectDB()
   .then(() => {
     console.log('Database connection established');
