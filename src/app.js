cda9332eeb7b3edcefbd848707ac6aea60308f9c
Sainath Shinde <shindesainath692@gmail.com>
2026-02-02 21:55:45 +0530

    Patch Method with handling return document

--- src/app.js
+++ src/app.js
@@ -64,6 +64,39 @@ app.get('/userById', async (req, res) => {
   }
 });
 
+// Delete user
+
+app.delete('/user', async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    // const user = await User.findByIdAndDelete({_id : userId}); or
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      res.status(201).send('User Deleted successfully');
+    }
+  } catch (error) {
+    res.status(400).send('User not found');
+  }
+});
+
+app.patch('/user', async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: 'after', // defaukt is before, now log will return after db update data
+    });
+
+    console.log('Userr', user);
+    res.status(201).send('User updated successfully');
+  } catch (error) {
+    res.status(401).send('user Update failed');
+  }
+});
+
 connectDB()
   .then(() => {
     console.log('Database connection established');
