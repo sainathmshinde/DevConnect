f21dbeb04d45ad16e1c6a388f8533d13293d8615
Sainath Shinde <shindesainath692@gmail.com>
2026-02-05 09:24:19 +0530

    API Validation update allowed fields

--- src/app.js
+++ src/app.js
@@ -81,11 +81,19 @@ app.delete('/user', async (req, res) => {
   }
 });
 
-app.patch('/user', async (req, res) => {
-  const userId = req.body.userId;
+app.patch('/user/:userId', async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
 
   try {
+    const ALLOWED_UPDATES = ['photoUrl', 'about', 'gender', 'age', 'skills'];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error('Update not allowed');
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: 'after', // defaukt is before, now log will return after db update data
       runValidators: true,
