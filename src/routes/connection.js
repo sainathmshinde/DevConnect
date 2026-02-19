bbf8d0d3a92da09d4386cb0e06b0f538a657c7a5
Sainath Shinde <shindesainath692@gmail.com>
2026-02-19 08:49:31 +0530

    Connection request api handling

--- src/routes/connection.js
+++ src/routes/connection.js
@@ -3,6 +3,7 @@ const connectionRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 connectionRouter.post(
   '/request/send/:status/:toUserId',
@@ -11,6 +12,31 @@ connectionRouter.post(
     const fromUserId = req.user._id;
     const toUserId = req.params.toUserId;
     const status = req.params.status;
+
+    const allowedStatus = ['ignored', 'interested'];
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: 'Inavalid status',
+        status,
+      });
+    }
+
+    const toUser = await User.findById(toUserId);
+    if (!toUser) {
+      res.status(400).json({ message: 'User not found!' });
+    }
+
+    const existingConnection = await ConnectionRequest.findOne({
+      $or: [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+    });
+    if (existingConnection) {
+      return res
+        .status(400)
+        .json({ message: 'Connection request already exist' });
+    }
 
     const connectionRequest = new ConnectionRequest({
       fromUserId,
